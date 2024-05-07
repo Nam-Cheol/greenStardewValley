20c7be807b887f4070d5e4087038c58e126fc813
Nam-Cheol <namcher9428@gmail.com>
2024-05-07 20:39:40 +0900

    Initial commit

---
+++ src/greenStardewValley/exam.java
@@ -0,0 +1,9 @@
+package greenStardewValley;
+
+public class exam {
+
+	public static void main(String[] args) {
+		System.out.println("1조");
+	}
+	
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module greenStardewValley {
+}
