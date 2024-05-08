a2183b17c4173ce1bd3f7e6d855397aece0890d5
knc <namcher9428@gmail.com>
2024-05-08 14:41:50 +0900

    플레이어 움직임 추가, KNC

--- src/stardewValley/frame/StardewValleyFrameKNC.java
+++ src/stardewValley/frame/StardewValleyFrameKNC.java
@@ -1,8 +1,13 @@
 package stardewValley.frame;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+
+import stardewValley.player.PlayerKnc;
 
 //TODO 생성자가 호출될 때 플레이어가 생성되어야 함.
 // 배경 추가해야 됨.
@@ -10,7 +15,7 @@ import javax.swing.JLabel;
 public class StardewValleyFrameKNC extends JFrame {
 	
 	private JLabel backgroundMap;
-	private JLabel status;
+	private PlayerKnc playerKnc;
 
 	public StardewValleyFrameKNC() {
 		initData();
@@ -20,21 +25,90 @@ public class StardewValleyFrameKNC extends JFrame {
 	
 	private void initData() {
 		backgroundMap = new JLabel(new ImageIcon("img/backgroundMap.png"));
-		status = new JLabel(new ImageIcon("img/status.png"));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setContentPane(backgroundMap);
-		setSize(1280, 980);
+		setSize(1930, 980);
+		
+		playerKnc = new PlayerKnc();
 	}
 	
 	private void setInitLayout() {
 		setLayout(null);
-//		setResizable(false);
+		setResizable(false);
 		setLocationRelativeTo(null);
 		setVisible(true);
+		
+		add(playerKnc);
 	}
 	
 	private void addEventListener() {
-		
+		addKeyListener(new KeyListener() {
+			
+			@Override
+			public void keyTyped(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void keyReleased(KeyEvent e) {
+
+				switch (e.getKeyCode()) {
+				case KeyEvent.VK_LEFT:
+					// 왼쪽으로 가능 상태 멈춤
+					playerKnc.setLeft(false);
+					playerKnc.setIcon(playerKnc.getPlayerL());
+					break;
+				case KeyEvent.VK_RIGHT:
+					// 오른쪽으로 가능 상태 멈춤
+					playerKnc.setRight(false);
+					playerKnc.setIcon(playerKnc.getPlayerR());
+					break;
+				case KeyEvent.VK_UP:
+					playerKnc.setUp(false);
+					playerKnc.setIcon(playerKnc.getPlayerUp());
+					break;
+				case KeyEvent.VK_DOWN:
+					playerKnc.setDown(false);
+					playerKnc.setIcon(playerKnc.getPlayerDown());
+					break;
+				default:
+					break;
+				}
+				
+			}
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				switch (e.getKeyCode()) {
+				case KeyEvent.VK_LEFT:
+					if(!playerKnc.isLeft()) {
+						playerKnc.left();
+					}
+					break;
+				case KeyEvent.VK_RIGHT:
+					if(!playerKnc.isRight()) {
+						playerKnc.right();
+					}
+					break;
+				case KeyEvent.VK_UP:
+					if(!playerKnc.isUp()) {
+						playerKnc.up();
+					}
+					break;
+				case KeyEvent.VK_DOWN:
+					if(!playerKnc.isDown()) {
+						playerKnc.down();
+					}
+					break;
+				default:
+					break;
+				}
+			}
+		});
 	}
 	
+	public static void main(String[] args) {
+		new StardewValleyFrameKNC();
+	}
 }
---
+++ src/stardewValley/player/PlayerWay.java
@@ -0,0 +1,6 @@
+package stardewValley.player;
+
+// 왼쪽, 오른쪽 <--- 범주화
+public enum PlayerWay {
+	LEFT, RIGHT, UP, DOWN
+}
