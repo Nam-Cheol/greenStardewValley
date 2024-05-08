7d9ddcb21d51bc5d8bf1f43754e9ca90239b49d9
knc <namcher9428@gmail.com>
2024-05-08 12:20:14 +0900

    2024-05-08

---
+++ src/stardewValley/player/playerCYJ.java
@@ -0,0 +1,19 @@
+package stardewValley.player;
+
+//TODO 플레이어의 기능 추가, 포함관계여야 함
+public class playerCYJ {
+
+	public playerCYJ() {
+		initData();
+		setInitLayout();
+	}
+
+	private void initData() {
+		// 브랜치 테스트 2 
+	}
+
+	private void setInitLayout() {
+		// 브랜치 테슽 
+	}
+
+}
---
+++ src/stardewValley/player/playerKnc.java
@@ -0,0 +1,19 @@
+package stardewValley.player;
+
+//TODO 플레이어의 기능 추가, 포함관계여야 함
+public class playerKnc {
+
+	public playerKnc() {
+		initData();
+		setInitLayout();
+	}
+
+	private void initData() {
+		// 브랜치 테스트 2 
+	}
+
+	private void setInitLayout() {
+		// 브랜치 테슽 
+	}
+
+}
