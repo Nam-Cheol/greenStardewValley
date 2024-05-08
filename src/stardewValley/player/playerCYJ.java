8ec7afb70cc8d346b564ca8fc52b6af87dd71dba
CYJ <wjdtpdbs1887@naver.com>
2024-05-08 12:33:09 +0900

    타입설정

--- src/stardewValley/player/playerCYJ.java
+++ src/stardewValley/player/playerCYJ.java
@@ -2,7 +2,17 @@ package stardewValley.player;
 
 //TODO 플레이어의 기능 추가, 포함관계여야 함
 public class playerCYJ {
-
+	
+	private int x;
+	private int y;
+	// 이미지 icon 변수들
+	
+	private boolean left;
+	private boolean right;
+	private boolean up;
+	private boolean down;
+	
+	
 	public playerCYJ() {
 		initData();
 		setInitLayout();
